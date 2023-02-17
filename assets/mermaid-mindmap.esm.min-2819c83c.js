3f8bcb885e6dad4f225274a2bf506fcb7b636fda
open-nan <54296915+open-nan@users.noreply.github.com>
2023-02-17 08:29:20 +0000

    Deploying to gh-pages from @ export-nan/export-nan.github.io@0bcf734818a418a2e39b75439f3503d36650cc6a 🚀

---
+++ assets/mermaid-mindmap.esm.min-2819c83c.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app-d3f14bea.js";import"./framework-2c2ce755.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-268cad43.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};
